7ff7d2a3a447bc490f9088606629bdcd823d1070
anirudhreddy18 <44629211+anirudhreddy18@users.noreply.github.com>
2020-03-26 13:49:30 -0500

    Update play.java

--- play.java
+++ play.java
@@ -1,26 +1,28 @@
 class Playground {
     public static void main(String[ ] args) {
-       String s1= "hello";
-       //string s2= "hello"  //creates in a string pool so result will be true here
-  
-       String s2= new String("hello"); //always create a new string the heap
-       
-       System.out.println(s1 == s2); //false
-       
-      s2 = s1.intern(); //points the strings to same address of s1
-      System.out.println(s1 == s2); //true
-    
-       changeValue(s2);
-       
-       System.out.println(s2); // still prints hello because strings are immutable
-       
-       
+        String s1= "hello";
+        String s2= "hello";  //creates in a string on stack so result will be true here
+
+        System.out.println(s1 == s2); //true
+
+        String s3= new String("hello"); // creates a new string the heap
+        String s4= new String("hello");
+        System.out.println(s3 == s4); //false
+
+        s4 = s2.intern(); //points the strings to same address of s3
+        System.out.println(s4 == s2); //true
+
+        changeValue(s2);
+
+        System.out.println(s2); // still prints hello because strings are immutable
+
+
     }
-    
+
     public static void changeValue(String s){
         s = "Hi";
         //creates a new string HELLO and points to that in heap
     }
-    
-    
+
+
 }
