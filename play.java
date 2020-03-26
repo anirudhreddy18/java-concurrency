cb0efd6dc91cb72bf97eea6e340f19152bf8eb3f
anirudhreddy18 <44629211+anirudhreddy18@users.noreply.github.com>
2020-03-26 14:10:50 -0500

    Update play.java

--- play.java
+++ play.java
@@ -1,13 +1,18 @@
 class Playground {
     public static void main(String[ ] args) {
         String s1= "hello";
-        String s2= "hello";  //creates in a string on stack so result will be true here
+        String s2= "hello";  
 
+        //creates a string on same string pool so result will be true here
         System.out.println(s1 == s2); //true
 
         String s3= new String("hello"); // creates a new string the heap
-        String s4= new String("hello");
+        String s4= new String("hello"); // creates a new string the heap
         System.out.println(s3 == s4); //false
+        
+        String s5= new String("hello"); // creates a new string the heap
+        String s6= s5; //allocates same address
+        System.out.println(s5 == s6); //true
 
         s4 = s2.intern(); //points the strings to same address of s3
         System.out.println(s4 == s2); //true
